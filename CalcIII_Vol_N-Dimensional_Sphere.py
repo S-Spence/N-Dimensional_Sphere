4106b5897fafe446614976984a7a12ac0ece7fec
S-Spence <sarah.spence1991@gmail.com>
2021-10-11 11:43:21 -0400

    Correcting typo

--- CalcIII_Vol_N-Dimensional_Sphere.py
+++ CalcIII_Vol_N-Dimensional_Sphere.py
@@ -1,6 +1,6 @@
 """This script was used for a discussion in multivariable calculus. The discussion prompt asked us to consider why a sphere's volume 
 decreases beyond 5-dimensions in multi-dimensional space. We were also asked to calculate the surface area of the sphere in n-dimensions.
-I used python to automate these calculations with two recurisive algorithms."""
+I used python to automate these calculations with two recursive algorithms."""
 
 import math
 
